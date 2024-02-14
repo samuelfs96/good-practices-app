548f05e9e6717a77faa3535128574e7956712f0d
Samuel <samuelfs96@gmail.com>
2024-02-14 03:09:29 -0400

    feat: add some styles

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,30 @@ import { Navbar } from "./components";
 import { Home } from "./pages";
 import { LayoutContainer } from "./styled-components";
 import store from "./redux/store";
+import { teal } from "@mui/material/colors";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: teal[400],
+    },
+    secondary: {
+      main: teal[100],
+    },
+  },
+});
 
 function App() {
   return (
-    <Provider store={store}>
-      <Navbar />
-      <LayoutContainer>
-        <Home />
-      </LayoutContainer>
-    </Provider>
+    <ThemeProvider theme={theme}>
+      <Provider store={store}>
+        <Navbar />
+        <LayoutContainer>
+          <Home />
+        </LayoutContainer>
+      </Provider>
+    </ThemeProvider>
   );
 }
 
--- src/styled-components/layout.styled.components.ts
+++ src/styled-components/layout.styled.components.ts
@@ -1,9 +1,7 @@
 import styled from "styled-components";
 
 export const LayoutContainer = styled.div`
-    @media only screen and (min-width: 1421px) {
-        width: 100%;
-    }
+    width: 80vw;
     @media only screen and (max-width: 1176px) {
         padding: 12px;
     }
