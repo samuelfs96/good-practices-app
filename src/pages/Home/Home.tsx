1c84908e5421267dc48376e95d83fcadae20e71d
Samuel <samuelfs96@gmail.com>
2024-02-13 22:20:56 -0400

    feat: add favorites logic

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,14 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import { DataGrid, GridRenderCellParams } from "@mui/x-data-grid";
 import { People } from "@/data/people";
+import { Person } from "@/models";
+import { Checkbox } from "@mui/material";
 
 export type HomeProps = {
   // types...
 };
 
 const Home: React.FC<HomeProps> = () => {
+  const [selectedPeople, setSelectedPeople] = useState<Person[]>([]);
   const pageSize = 5;
+  const findPerson = (person: Person) =>
+    !!selectedPeople.find((p) => p.id === person.id);
+  const filterPerson = (person: Person) =>
+    selectedPeople.filter((p) => p.id !== person.id);
+  const handleChange = (person: Person) => {
+    setSelectedPeople(
+      findPerson(person) ? filterPerson(person) : [...selectedPeople, person]
+    );
+  };
   const columns = [
+    {
+      field: "actions",
+      type: 'actions',
+      sortable: false,
+      headerName: "",
+      width: 50,
+      renderCell: (params: GridRenderCellParams) => (
+        <>
+          {
+            <Checkbox
+              size="small"
+              checked={findPerson(params.row)}
+              onChange={() => handleChange(params.row)}
+            />
+          }
+        </>
+      ),
+    },
     {
       field: "name",
       headerName: "Name",
@@ -32,17 +62,17 @@ const Home: React.FC<HomeProps> = () => {
     },
   ];
   return (
-      <DataGrid
-        rows={People}
-        columns={columns}
-        disableColumnSelector
-        disableRowSelectionOnClick
-        autoHeight
-        initialState={{
-          pagination: { paginationModel: { pageSize: pageSize } },
-        }}
-        pageSizeOptions={[pageSize]}
-      />
+    <DataGrid
+      rows={People}
+      columns={columns}
+      disableColumnSelector
+      disableRowSelectionOnClick
+      autoHeight
+      initialState={{
+        pagination: { paginationModel: { pageSize: pageSize } },
+      }}
+      pageSizeOptions={[pageSize]}
+    />
   );
 };
 
